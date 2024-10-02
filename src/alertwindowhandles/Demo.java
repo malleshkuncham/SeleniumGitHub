42cda8c44d18fd8ff2259cc87981e81a0a508ccc
malleshkuncham <reddymalleshwarkuncham209@gmail.com>
2024-10-02 16:26:36 +0530

    Update Demo.java

--- src/alertwindowhandles/Demo.java
+++ src/alertwindowhandles/Demo.java
@@ -2,14 +2,18 @@ package alertwindowhandles;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class Demo {
+	WebDriver driver;
   @Test
-  public void f() {
-  }
+  public void techlearn() {
+	  driver.get("https://www.techlearn.in");  }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
   }
 
   @AfterTest
