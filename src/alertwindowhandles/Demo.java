33164ae89143ace93a97691709a209a95ec32abb
malleshkuncham <reddymalleshwarkuncham209@gmail.com>
2024-10-02 16:22:33 +0530

    New demo lass added under

---
+++ src/alertwindowhandles/Demo.java
@@ -0,0 +1,19 @@
+package alertwindowhandles;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Demo {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
