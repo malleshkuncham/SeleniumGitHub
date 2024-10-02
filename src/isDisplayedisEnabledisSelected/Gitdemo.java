e5d4b6e6daee818dff751ffaaf9b55ee82dfaa15
malleshkuncham <reddymalleshwarkuncham209@gmail.com>
2024-10-02 16:38:43 +0530

    GitDemo Class added under isDispalyed package

---
+++ src/isDisplayedisEnabledisSelected/Gitdemo.java
@@ -0,0 +1,14 @@
+package isDisplayedisEnabledisSelected;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class Gitdemo {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
